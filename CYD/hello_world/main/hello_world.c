08f966b78bc3f653727330beed618d72a7e3ed15
Alexander <rednaxela@tuta.io>
2026-03-01 21:18:27 +0100

    Making basic helloworld

--- CYD/hello_world/main/hello_world.c
+++ CYD/hello_world/main/hello_world.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <stdio.h>
 
-void app_main(void)
-{
-    int count = 0;
-    while (count <= 4)
-    {
-        printf("%s\n", "Hello World");
+void app_main(void) {
+  int count = 0;
+  while (count <= 4) {
+    printf("%s\n", "Hello World");
 
-        // Delay mili
-        vTaskDelay(pdMS_TO_TICKS(1000));
-        count++;
-    }
+    // Delay mili
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    count++;
+  }
 }
